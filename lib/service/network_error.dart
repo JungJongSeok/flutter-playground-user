3262bea8e159ee46c6039d0d8467e298dfc6c9b7
Loco <ee5188@naver.com>
2025-07-06 21:45:35 +0900

    Fix a import absolute path (#7)
    * Fix a import absolute path
    
    * Fix a scroll logic

--- lib/service/network_error.dart
+++ lib/service/network_error.dart
@@ -5,11 +5,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:json_annotation/json_annotation.dart';
-import 'package:playground/extensions/collection.dart';
-import 'package:playground/service/response/error_response.dart';
 
+import '../extensions/collection.dart';
 import '../utils/custom_widget.dart';
 import 'base_service.dart';
+import 'response/error_response.dart';
 
 part 'network_error.g.dart';
 
